b5272ee55265078582761813179bef83375663c7
Dinosaure <amin.touimer@ulb.be>
2025-10-29 11:03:25 +0100

    edit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib as m
 import numpy as n
+from google.cloud import speech_v2 as speech
 
 from controller.controller import Controller
 from view.view import View
